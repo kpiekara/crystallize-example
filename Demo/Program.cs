58dbcd3f9771de5d4ba5d8fefb5737ed8ba90d28
Kamil Piekara <kamil.piekara@madmax.consulting>
2023-06-11 12:15:06 +0200

    Add client call

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Demo;
+using Microsoft.Extensions.DependencyInjection;
+using StrawberryShake;
+
+var serviceCollection = new ServiceCollection();
+
+serviceCollection
+    .Addcatalogue()
+    .ConfigureHttpClient(client => client.BaseAddress = new Uri("https://workshop.chillicream.com/graphql"));
+
+IServiceProvider services = serviceCollection.BuildServiceProvider();
+
+var client = services.GetRequiredService<Icatalogue>();
+
+var result = await client.GetItem.ExecuteAsync("", "");
+result.EnsureNoErrors();
+
+Console.WriteLine(result.Data.Catalogue);
